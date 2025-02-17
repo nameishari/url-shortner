de78f102b6ec977036adcc8ab4a9e23d9a8ffcca
nameishari <hari@element.in>
2025-02-17 19:04:30 +0100

    add retry mechanism to handle concurrent requests

--- src/main/kotlin/org/challenge/urlshorteningservice/service/UrlShortnerService.kt
+++ src/main/kotlin/org/challenge/urlshorteningservice/service/UrlShortnerService.kt
@@ -6,12 +6,23 @@ import org.challenge.urlshorteningservice.exception.NotFoundException
 import org.challenge.urlshorteningservice.model.ShortUrl
 import org.challenge.urlshorteningservice.repository.ShortUrlRepository
 import org.challenge.urlshorteningservice.util.isValidUrl
+import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 
 @Service
 class UrlShortnerService(private val repository: ShortUrlRepository) {
 
     @Transactional
+    // just to ensure that concurrent requests are processed well.
+    // if two requests are made of one url at once, there might be chance of throwing 500 due to unique constraint on database level.
+    // Could only retry for unique constraint but for now keeping it simpler
+    @Retryable(
+        retryFor = [DataIntegrityViolationException::class],
+        maxAttempts = 3,
+        backoff = Backoff(delay = 1000)
+    )
     fun shortenUrl(originalUrl: String): String {
         if (!originalUrl.isValidUrl()) {
             throw BadRequestException("Invalid URL")
