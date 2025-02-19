448726116273d29e6bea46bb6cc60689ab00766c
nameishari <hari@element.in>
2025-02-19 05:20:24 +0100

    clean up

--- src/main/kotlin/org/challenge/urlshorteningservice/api/model/UrlShortnerRequest.kt
+++ src/main/kotlin/org/challenge/urlshorteningservice/api/model/UrlShortnerRequest.kt
@@ -1,7 +1,5 @@
 package org.challenge.urlshorteningservice.api.model
 
-import org.hibernate.validator.constraints.URL
-
 data class UrlShortnerRequest(
-    @URL val url: String,
+    val url: String,
 )
